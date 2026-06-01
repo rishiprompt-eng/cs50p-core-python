a3b49502596817eea7aebf95710faf345b048dcc
rishiprompt-eng <kondahrishikesh@gmail.com>
2026-06-01 10:52:05 +0000

    Mon, Jun 1, 2026, 4:22 PM +05:30

--- faces/faces.py
+++ faces/faces.py
@@ -1,7 +1,6 @@
 def convert():
         return input("enter text: ").replace(":)", "🙂").replace(":(", "🙁")
 def main():
-    x = input("enter text: ")
 
-print(convert())
+    print(convert())
 main()
